27a38cb426d68c7fe91244b9c1c5da4042a60994
ylxwyjj <147592352+ylxwyjj@users.noreply.github.com>
2023-10-22 14:42:32 +0800

    Update config_handler.py

--- config_handler.py
+++ config_handler.py
@@ -1,14 +1,11 @@
 import configparser
 
 def load_config(filename='config.ini'):
-    with open(filename, 'r') as file:
-        print(file.read())
     config = configparser.ConfigParser()
     config.read(filename)
-    return {
-        'api_key': config.get('default', 'api_key'),
-        'address': config.get('default', 'address'),
-        'temperature': config.getfloat('default', 'temperature'),
-        'model': config.get('default', 'model'),
-        'system_role': config.get('default', 'system_role')
-    }
+
+    attributes = dict(config['default'])
+    # The temperature is a float, not str.
+    attributes['temperature'] = float(attributes['temperature'])
+    print(attributes)
+    return attributes
